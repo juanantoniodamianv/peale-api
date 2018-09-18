91829406153b3318a2680317da4bc40c9d492c2c
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2018-09-18 10:52:43 -0300

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+# Node JS LTS version, Jul 4th 2018
+FROM keymetrics/pm2:8-alpine
+
+LABEL maintainer="https://ballastlane.com/"
+
+ENV SCRIPT start
+
+# Go to /var/node
+WORKDIR /var/node
+
+# Copy package.json and package-lock.json to current working directory
+COPY package*.json ./
+
+# Run the application
+CMD ["sh", "-c", "npm $SCRIPT"]
+
+# Expose backend default port
+EXPOSE 1337
---
+++ api/models/Sermon.js
@@ -0,0 +1,46 @@
+/**
+ * Sermon.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    fileName: {},
+
+    date: {},
+
+    title: {},
+
+    description: {},
+
+    bibleVerse: {},
+
+    bibleVerseText: {},
+
+    type: {},
+
+    duration: {},
+
+    package: {},
+
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
