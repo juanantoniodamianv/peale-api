faa018b6a6d6af44da09d6c63a4e5278710c6b5a
ivaniguaran <developer@ballastlane.com>
2018-10-30 16:00:32 -0500

    Add Tags behavior to Favorites list response

--- api/controllers/v1/tag/index.js
+++ api/controllers/v1/tag/index.js
@@ -24,14 +24,10 @@ module.exports = {
     },
   },
 
-
   fn: async function (inputs, exits) {
     var tags = await Tag.find({});
-
     if (!tags) throw { unauthorized: 'Unauthorized request.' };
-
     return exits.success(tags);
   }
-
 
 };
